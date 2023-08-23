b62694ada74fbbbbcb5819efa19a57ec3c566a6f
0xsuk <iam0xsuk@gmail.com>
2023-08-23 20:11:09 +0900

    -<> to flow

--- clap.lisp
+++ clap.lisp
@@ -11,7 +11,21 @@
     (white-noise.ar)
     (hpf.ar <> 600)
     (lpf.ar <> (x-line.kr 7200 4000 .03))
-    (bpf.ar <> 1620 3)
+    (bpf.ar <> 1620 3) :as noise1
+
+    (env
+     '(0 1 0)
+     '(.02 .18)
+     '(0 -4))
+    (env-gen.ar <> :act :free)
+    (white-noise.ar)
+    (hpf.ar <> 1000)
+    (lpf.ar <> 7600)
+    (bpf.ar <> 1230 .7 .7) :as noise2
+    
+    (+ noise1 noise2)
+    (* 2)
+    (softclip)
     (draw)
     (out.ar 0)
     )
